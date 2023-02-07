0f0c8d27dd1e16b88a0b2951b2123b93dc3c0345
Ivan <116812839+IvanYakovenko75@users.noreply.github.com>
2023-02-07 16:59:25 +0200

    Import assets

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
